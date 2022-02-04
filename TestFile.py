103b6ef98784fc1e48deef2bfbc6ee93ae6a55f5
Gabriel <83326462+GabrielC-34@users.noreply.github.com>
2022-02-04 14:21:53 -0500

    Ajout de l'importation de Webcam
    -Import de webcam
    -Affichage du flux vidéo en direct
    -Mise à jour du .gitignore

--- TestFile.py
+++ TestFile.py
@@ -1,7 +1,13 @@
 import cv2 #Importe la librairie OpenCV
-imgInitiale = cv2.imread('Image/imageTest.png', 1) #Lis le fichier PNG de l'image dans le dossier 'Image'
-imgFinale = cv2.resize(imgInitiale, (600,600)) #Resize l'image à une dimension de 600x600 au lieu de 400x400
-imgInitiale = cv2.rotate(imgFinale, cv2.ROTATE_90_CLOCKWISE) #Rotate l'image de 90 degrés dans le sens des aiguilles d'une montre
-cv2.imshow('Image de test', imgInitiale) #Affiche l'image dans une fenêtre ayant pour titre 'Image de test'
-cv2.waitKey(0) #Attends indéfiniment que l'utilisateur clique sur une touche quelconque du clavier
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    print('Oh no!')
+while True:
+    ret,frame = cam.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+cam.release()
 cv2.destroyAllWindows #Ferme la fenêtre

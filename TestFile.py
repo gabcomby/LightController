5e7e6143a4efa1601753b23218dfcda6e2876f14
Gabriel <83326462+GabrielC-34@users.noreply.github.com>
2022-02-04 14:26:29 -0500

    Ajout de l'importation de Webcam
    -Import de webcam
    -Affichage du flux vidéo en direct
    -Mise à jour du .gitignore

--- TestFile.py
+++ TestFile.py
@@ -3,11 +3,11 @@ cam = cv2.VideoCapture(0)
 if not cam.isOpened():
     print('Oh no!')
 while True:
-    ret,frame = cam.read()
+    ret,frame = cam.read() #ret est un boolean, frame est une image
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
     if c == 27:
         break
-cam.release()
-cv2.destroyAllWindows #Ferme la fenêtre
+cam.release() #Ferme la caméra
+cv2.destroyAllWindows
